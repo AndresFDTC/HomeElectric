b65031e181ba733fb5c1a0bda9febe2c2568fe12
Jorge <jorandho@gmail.com>
2022-09-21 09:35:44 -0500

    Adición del filtro para que el usuario solo pueda ver los alquileres propios

--- home_electric_project/alquiler/urls.py
+++ home_electric_project/alquiler/urls.py
@@ -4,12 +4,12 @@ from .views import *
 urlpatterns = [
     path('reg_tool/', RegisterTool.as_view(), name='register_tool'),
     path('list_tool/', ListTool.as_view(), name='list_tool'),
-    path('retrieve_tool/<pk>', RetrieveTool.as_view(), name='retrieve_tool'),
-    path('update_tool/<pk>', UpdateTool.as_view(), name='update_tool'),
-    path('delete_tool/<pk>', DeleteTool.as_view(), name='delete_tool'),
+    path('retrieve_tool/<int:pk>', RetrieveTool.as_view(), name='retrieve_tool'),
+    path('update_tool/<int:pk>', UpdateTool.as_view(), name='update_tool'),
+    path('delete_tool/<int:pk>', DeleteTool.as_view(), name='delete_tool'),
     path('reg_rent/', RegisterRent.as_view(), name='register_rent'),
     path('list_rent/', ListRent.as_view(), name='list_rent'),
-    path('retrieve_rent/<pk>', RetrieveRent.as_view(), name='retrieve_rent'),
-    path('update_rent/<pk>', UpdateRent.as_view(), name='update_rent'),
-    path('delete_rent/<pk>', DeleteRent.as_view(), name='delete_rent'),
+    path('retrieve_rent/<int:pk>', RetrieveRent.as_view(), name='retrieve_rent'),
+    path('update_rent/<int:pk>', UpdateRent.as_view(), name='update_rent'),
+    path('delete_rent/<int:pk>', DeleteRent.as_view(), name='delete_rent'),
 ]
