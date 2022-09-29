c95df25bd59284dc82ac6cb5d7bcb639f99b3fc6
Jorge <jorandho@gmail.com>
2022-09-28 22:18:09 -0500

    creación api para obtener id y first name del usuario

--- home_electric_project/perfil/views.py
+++ home_electric_project/perfil/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import generics, status
 from django.contrib.auth.models import User
@@ -14,3 +14,9 @@ class RegisterList(generics.ListAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+class RetrieveUser(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+    lookup_field = 'username'
