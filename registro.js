fe0913254df8f5129089c1b0f533cf73f78e9ce3
Jorge <jorandho@gmail.com>
2022-10-03 16:49:19 -0500

    correccion

--- registro.js
+++ registro.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         var nom = $("#first_name").val();
         var ape = $("#last_name").val();
 
-        const url = "https://andresfdtc.github.io/HomeElectric/home_electric_project/perfil/registro/"
+        const url = "https://andresfdtc.github.io/HomeElectric/perfil/registro/"
 
         var data = {
             "username": nomUs,
