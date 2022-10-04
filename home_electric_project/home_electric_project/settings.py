26bd8fa6c0ef226d43fffd11fa6ac4decbfdb807
Jorge <jorandho@gmail.com>
2022-10-04 13:12:11 -0500

    max con age

--- home_electric_project/home_electric_project/settings.py
+++ home_electric_project/home_electric_project/settings.py
@@ -94,6 +94,8 @@ WSGI_APPLICATION = 'home_electric_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+MAX_CONN_AGE = 600
+
 DATABASES = {
     'default': {
         'ENGINE': dj_database_url.config(
