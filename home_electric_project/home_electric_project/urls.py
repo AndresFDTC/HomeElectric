a65535c38bccd09d131a5054680bc76f3a48f6c6
Jorge <jorandho@gmail.com>
2022-10-06 11:25:08 -0500

    ensayo url admin

--- home_electric_project/home_electric_project/urls.py
+++ home_electric_project/home_electric_project/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', include(admin.site.urls)),
     path('perfil/', include('perfil.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('alquiler/', include('alquiler.urls')),
